8caafd22f41c994e5ab0b3b384b809446990415d
fhnstephen <fhnstephen@gmail.com>
2023-06-14 14:47:24 +0800

    to lowercase

--- entrypoint.sh
+++ entrypoint.sh
@@ -59,8 +59,9 @@ rm -rf ./${INPUT_ALLURE_RESULTS}/history
 echo "downloading latest history from s3"
 mkdir -p ./${INPUT_ALLURE_RESULTS}/history
 # INPUT_LATEST_DEST looks like "Desktop Chrome"
-# convert INPUT_LATEST_DEST to URL safe string
-LATEST_URL=$(echo ${INPUT_LATEST_DEST} | sed 's/ /-/g')
+# convert INPUT_LATEST_DEST to URL safe string, lowercase, replace spaces with dashes
+# example: "Desktop Chrome" -> "desktop-chrome"
+LATEST_URL=$(echo ${INPUT_LATEST_DEST} | tr '[:upper:]' '[:lower:]' | sed 's/ /-/g')
 sh -c "aws s3 cp s3://${AWS_S3_BUCKET}/${LATEST_URL}/history ./${INPUT_ALLURE_RESULTS}/history \
               --no-progress \
               --recursive"
